517572719ee2abee93f50a21b8216a2860b82afd
Jakub Jantosik <jakub.jantosik@philips.com>
2019-05-30 23:17:18 +0200

    VMA vector update

--- src/vulkan/memory/allocator/lib/vma_vector.h
+++ src/vulkan/memory/allocator/lib/vma_vector.h
@@ -1,5 +1,5 @@
-#ifndef VLC_VECTOR_H
-#define VLC_VECTOR_H
+#ifndef VMA_VECTOR_H
+#define VMA_VECTOR_H
 
 #include <stdbool.h>
 #include <stddef.h>
@@ -41,6 +41,10 @@ static inline size_t vma_vector_between_(size_t x, size_t min, size_t max) {
     return vma_vector_max_(min, vma_vector_min_(max, x));
 }
 
+static inline size_t vma_vector_enforce_size_t_(size_t value) {
+    return value;
+}
+
 #define VMA_VECTOR_FAILFLAG_ (~(((size_t) -1) >> 1))
 
 static inline void* vma_vector_reallocdata_(void* ptr, size_t count, size_t size, size_t* restrict pcap,
@@ -67,6 +71,32 @@ static inline bool vma_vector_test_and_reset_failflag_(size_t *pcap) {
 #define vma_vector_realloc_(pv, new_cap) (                                                                   \
     (pv)->data = vma_vector_reallocdata_((pv)->data, new_cap, sizeof(*(pv)->data), &(pv)->cap, &(pv)->size), \
     !vma_vector_test_and_reset_failflag_(&(pv)->cap)                                                         \
+)
+
+#define vma_vector_resize_(pv, new_cap) ( \
+    (pv)->cap == (new_cap) ||  \
+    ( \
+        (new_cap) > 0 ? vma_vector_realloc_(pv, new_cap) : (vma_vector_clear(pv), true) \
+    ) \
+)
+
+static inline size_t vma_vector_growsize_(size_t value) {
+    // integer multiplication by 1.5
+    return value + (value >> 1);
+}
+
+#define vma_vector_max_cap_(pv) (SIZE_MAX / 2 / sizeof(*(pv)->data))
+
+#define vma_vector_reserve(pv, min_cap) \
+    vma_vector_reserve_internal_(pv, vma_vector_max_(min_cap, VMA_VECTOR_MINCAP_))
+
+#define vma_vector_reserve_internal_(pv, min_cap) ( \
+    (min_cap) <= (pv)->cap ((min_cap) <= vma_vector_max_cap_(pv) && \
+        vma_vector_realloc_(pv, \
+            vma_vector_between_(vma_vector_growsize_((pv)->cap), \
+                min_cap, vma_vector_max_cap_(pv)) \
+        ) \
+    ) \
 )
 
 #endif
