707e7b47700fbe15a6cf99aba544a2b81d5723d1
jammymalina <jakub.jantosik@gmail.com>
2019-06-26 23:26:29 +0200

    Updated unit test for hash map

--- test/test_hash_string_map.c
+++ test/test_hash_string_map.c
@@ -172,6 +172,113 @@ void test_hash_string_map_search_after_increased_capacity() {
     TEST_ASSERT_EQUAL_UINT64_MESSAGE(expected_size,
         hash_string_map_get_size(&m), "Size of a map should increase");
 
+    int32_t x = -1;
+    bool get_status = false;
+
+    get_status = hash_string_map_get(&m, "spring", &x);
+    TEST_ASSERT_MESSAGE(get_status, "Unable to get data from map");
+    TEST_ASSERT_EQUAL_INT32_MESSAGE(10, x, "Incorrect retrieved value");
+
+    get_status = hash_string_map_get(&m, "pepper", &x);
+    TEST_ASSERT_MESSAGE(get_status, "Unable to get data from map");
+    TEST_ASSERT_EQUAL_INT32_MESSAGE(42, x, "Incorrect retrieved value");
+
+    get_status = hash_string_map_get(&m, "roomba", &x);
+    TEST_ASSERT_MESSAGE(get_status, "Unable to get data from map");
+    TEST_ASSERT_EQUAL_INT32_MESSAGE(50, x, "Incorrect retrieved value");
+
+    get_status = hash_string_map_get(&m, "umbrella", &x);
+    TEST_ASSERT_MESSAGE(get_status, "Unable to get data from map");
+    TEST_ASSERT_EQUAL_INT32_MESSAGE(40, x, "Incorrect retrieved value");
+
+    get_status = hash_string_map_get(&m, "wheel", &x);
+    TEST_ASSERT_MESSAGE(get_status, "Unable to get data from map");
+    TEST_ASSERT_EQUAL_INT32_MESSAGE(30, x, "Incorrect retrieved value");
+
+    // we should not get false positives
+    get_status = hash_string_map_get(&m, "wheeel", &x);
+    TEST_ASSERT_FALSE_MESSAGE(get_status, "False get positive, element is not"
+        " the map");
+    get_status = hash_string_map_get(&m, "umbrellla", &x);
+    TEST_ASSERT_FALSE_MESSAGE(get_status, "False get positive, element is not"
+        " the map");
+    get_status = hash_string_map_get(&m, "key1", &x);
+    TEST_ASSERT_FALSE_MESSAGE(get_status, "False get positive, element is not"
+        " the map");
+    get_status = hash_string_map_get(&m, "somedummymummy", &x);
+    TEST_ASSERT_FALSE_MESSAGE(get_status, "False get positive, element is not"
+        " the map");
+    get_status = hash_string_map_get(&m, "rheel", &x);
+    TEST_ASSERT_FALSE_MESSAGE(get_status, "False get positive, element is not"
+        " the map");
+
+    hash_string_map_destroy(&m);
+}
+
+void test_hash_string_map_delete() {
+    hash_int_map m;
+    hash_string_map_init(&m);
+
+    bool status = hash_string_map_reserve(&m, 20);
+
+    TEST_ASSERT_MESSAGE(status, "Unable to reserve space for map");
+    bool add_status = hash_string_map_add(&m, "spring", 10) &&
+        hash_string_map_add(&m, "autumn", 20) &&
+        hash_string_map_add(&m, "wheel", 30) &&
+        hash_string_map_add(&m, "umbrella", 40) &&
+        hash_string_map_add(&m, "roomba", 50) &&
+        hash_string_map_add(&m, "salsa", 100) &&
+        hash_string_map_add(&m, "ball", 210) &&
+        hash_string_map_add(&m, "pepper", 42);
+    TEST_ASSERT_MESSAGE(add_status, "Unable to add data to map");
+
+    // should delete these items
+    bool delete_status = false;
+    bool search_status = false;
+
+    delete_status = hash_string_map_delete(&m, "roomba");
+    search_status = hash_string_map_has(&m, "roomba");
+    TEST_ASSERT_MESSAGE(delete_status, "Delete failed - item was not found");
+    TEST_ASSERT_EQUAL_UINT64_MESSAGE(7, hash_string_map_get_size(&m),
+        "Size of a map should decrease after deleting an item");
+    TEST_ASSERT_FALSE_MESSAGE(search_status, "The item is still in a table");
+
+    delete_status = hash_string_map_delete(&m, "salsa");
+    search_status = hash_string_map_has(&m, "salsa");
+    TEST_ASSERT_MESSAGE(delete_status, "Delete failed - item was not found");
+    TEST_ASSERT_EQUAL_UINT64_MESSAGE(6, hash_string_map_get_size(&m),
+        "Size of a map should decrease after deleting an item");
+    TEST_ASSERT_FALSE_MESSAGE(search_status, "The item is still in a table");
+
+    delete_status = hash_string_map_delete(&m, "ball");
+    search_status = hash_string_map_has(&m, "ball");
+    TEST_ASSERT_MESSAGE(delete_status, "Delete failed - item was not found");
+    TEST_ASSERT_EQUAL_UINT64_MESSAGE(5, hash_string_map_get_size(&m),
+        "Size of a map should decrease after deleting an item");
+    TEST_ASSERT_FALSE_MESSAGE(search_status, "The item is still in a table");
+
+    // should not delete an item that was already deleted
+    delete_status = hash_string_map_delete(&m, "roomba");
+    TEST_ASSERT_FALSE_MESSAGE(delete_status, "Delete should have failed");
+    TEST_ASSERT_EQUAL_UINT64_MESSAGE(5, hash_string_map_get_size(&m),
+        "Size of a map should have stayed the same");
+
+    delete_status = hash_string_map_delete(&m, "ball");
+    TEST_ASSERT_FALSE_MESSAGE(delete_status, "Delete should have failed");
+    TEST_ASSERT_EQUAL_UINT64_MESSAGE(5, hash_string_map_get_size(&m),
+        "Size of a map should have stayed the same");
+
+    // adding after delete should be fine
+    int32_t x;
+    bool get_status = false;
+    add_status = hash_string_map_add(&m, "roomba", 50);
+    get_status = hash_string_map_get(&m, "roomba", &x);
+    TEST_ASSERT_EQUAL_UINT64_MESSAGE(6, hash_string_map_get_size(&m),
+        "Size of a map should have increased");
+    TEST_ASSERT_MESSAGE(get_status, "Unable to add data after delete");
+    TEST_ASSERT_MESSAGE(get_status, "Unable to get data from map after delete");
+    TEST_ASSERT_EQUAL_INT32_MESSAGE(50, x, "Incorrect retrieved value");
+
     hash_string_map_destroy(&m);
 }
 
@@ -182,6 +289,7 @@ int main() {
     RUN_TEST(test_hash_string_map_insert);
     RUN_TEST(test_hash_string_map_get);
     RUN_TEST(test_hash_string_map_search_after_increased_capacity);
+    RUN_TEST(test_hash_string_map_delete);
 
     return UNITY_END();
 }
