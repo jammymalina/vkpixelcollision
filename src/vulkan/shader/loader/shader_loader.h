79e5668b8c153a43194b1e9f5d2b4856e4c4071e
jammymalina <jakub.jantosik@gmail.com>
2019-07-02 08:27:37 +0200

    Shader loader update

--- src/vulkan/shader/loader/shader_loader.h
+++ src/vulkan/shader/loader/shader_loader.h
@@ -1,10 +1,29 @@
 #ifndef SHADER_LOADER_Hx
 #define SHADER_LOADER_H
 
+#include <stddef.h>
+
 #include "../../gpu/gpu.h"
 #include "../shader_program/shader_types.h"
 
+#define DEFAULT_MAX_SHADER_PROGRAM_BYTE_SIZE (1024 * 1024)
+
+typedef struct shader_loader_create_info {
+    size_t max_shader_program_byte_size;
+} shader_loader_create_info;
+
+typedef struct shader_loader {
+    char* program_buffer;
+    size_t max_shader_program_byte_size;
+} shader_loader;
+
+void shader_loader_init_empty(shader_loader* shl);
+void shader_loader_init(shader_loader* shl, const shader_loader_create_info*
+    shl_config);
+
 bool shader_loader_load_shader(shader* shd, const char* filename,
     const char name[SHADER_MAX_NAME_SIZE], const gpu_info* gpu);
+
+void destroy_shader_loader();
 
 #endif
