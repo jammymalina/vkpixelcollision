1bc0208432f14b200d4755a9f1a801d95220fb06
jammymalina <jakub.jantosik@gmail.com>
2021-11-21 01:10:35 +0100

    First rust commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use ash::{vk, Entry};
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let entry = (unsafe { Entry::new() }).unwrap();
+    let app_info = vk::ApplicationInfo {
+        api_version: vk::make_api_version(0, 1, 2, 0),
+        ..Default::default()
+    };
+    let create_info = vk::InstanceCreateInfo {
+        p_application_info: &app_info,
+        ..Default::default()
+    };
+    let instance = (unsafe { entry.create_instance(&create_info, None) }).unwrap();
+
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                window_id,
+            } if window_id == window.id() => *control_flow = ControlFlow::Exit,
+            _ => (),
+        }
+    });
 }
